fe16b2915f49935229eba73f669eddd50d0e1662
katu-n <hieizan1509@gmail.com>
2024-07-31 16:53:43 +0900

    pageとsingleを作成

---
+++ js/single.js
@@ -0,0 +1,20 @@
+
+document.querySelector('.toggle__btn').addEventListener('click',function() {
+        document.querySelector('.l-humberger').classList.toggle('open');
+        document.querySelector('.l-humberger').classList.toggle('close');
+        document.querySelector('.l-humberger__toggle').classList.toggle('open');
+        document.querySelector('.l-humberger__fixed').classList.toggle('open');
+        document.querySelector('.l-humberger__btn').classList.toggle('open');
+        document.querySelector('.l-humberger__overlay').classList.toggle('open');
+    }
+);
+
+document.querySelector('.l-humberger__toggle').addEventListener('click',function() {
+        document.querySelector('.l-humberger').classList.toggle('open');
+        document.querySelector('.l-humberger').classList.toggle('close');
+        document.querySelector('.l-humberger__toggle').classList.toggle('open');
+        document.querySelector('.l-humberger__fixed').classList.toggle('open');
+        document.querySelector('.l-humberger__btn').classList.toggle('open');
+        document.querySelector('.l-humberger__overlay').classList.toggle('open');
+    }
+);
